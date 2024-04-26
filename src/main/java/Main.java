2e7f906f043a21b74f46dffc188a48e3447d23d5
Maksim Zharinov <minimum.cold1991@gmail.com>
2024-04-27 02:44:07 +0700

    done this task

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         BinOps bins = new BinOps();
+
+        //done this task
+        System.out.println(bins.mult("00101001", "11000101"));
+        System.out.println(bins.sum("10101010", "01010101"));
     }
 }
